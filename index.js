9e48cf2269e390755169183bec461d5537754ed6
Alessa Santos <alessa.bcc@gmail.com>
2022-05-30 22:15:40 -0300

    Para fazer o merge

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
 //importações
 import http from "http";
 import express from "express";
-import rotaProduto from "./rotas/rotaProdutos.js";
-import cors from 'cors';
-import rotaFuncionario from "./rotas/rotaFuncionario.js";
+import rotaCliente from "./rotas/rotaClientes.js";
 
 const hostname = "localhost";
 const porta = 3000;
 
 const app = express();
 
-//app.use(express.static('./public'));
-
-app.use(cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"]
-}));
-
-//Rota Produtos - Flavia
-app.use('/produtos', rotaProduto);
-//Rota Funcionarios - Alessa
-app.use('/funcionario',rotaFuncionario);
+//Rota Clientes - Junior
+app.use('/clientes', rotaCliente);
 
 const servidor = http.createServer(app);
 
 servidor.listen(porta, hostname, () => {
     console.log("Servidor rodando em http://%s:%d", hostname, porta);
 });
-
