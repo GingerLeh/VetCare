cb1342f0ac5c5cd35d73068e8e3a5fd792d4910b
Alessa Santos <alessa.bcc@gmail.com>
2022-05-30 22:24:41 -0300

    Pra fazer a develop

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 //importações
 import http from "http";
 import express from "express";
-<<<<<<< HEAD
 import rotaProduto from "./rotas/rotaProdutos.js";
 import cors from 'cors';
 import rotaFuncionario from "./rotas/rotaFuncionario.js";
-=======
 import rotaCliente from "./rotas/rotaClientes.js";
->>>>>>> 9e48cf2269e390755169183bec461d5537754ed6
 
 const hostname = "localhost";
 const porta = 3000;
 
 const app = express();
 
-<<<<<<< HEAD
 //app.use(express.static('./public'));
 
 app.use(cors({
@@ -26,10 +22,8 @@ app.use(cors({
 app.use('/produtos', rotaProduto);
 //Rota Funcionarios - Alessa
 app.use('/funcionario',rotaFuncionario);
-=======
 //Rota Clientes - Junior
 app.use('/clientes', rotaCliente);
->>>>>>> 9e48cf2269e390755169183bec461d5537754ed6
 
 const servidor = http.createServer(app);
 
